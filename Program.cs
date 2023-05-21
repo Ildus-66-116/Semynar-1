e75b563f55e733ce60e4f754ee15e33e757f8722
ILDUS <ildus6696@gmail.com>
2023-05-21 13:40:08 +0300

    Семинар 1 C#

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿
+/*
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+*/
+// ctrl + /(+ - это не клавиша, а одновременное нажатие ctrl и /)
+
+// Ввод чисел
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{n} + {m} = {n + m}");
+// желтое выделение - это предупреждение
+// красное выделение - это ошибка
+// $"" - интерполяция строк
+
+
+// Пользователь вводит 2 числа. Необходимо вывести наибольшее из них, в случае, если
+// числа равны, то предупредить об этом пользователя
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// // Фигурные скобки({}) ставятся тогда, когда внутри блока условия/цикла выполняется более 1 строчке кода
+// if (n > m)
+//     Console.WriteLine(n);
+// else if (n < m)
+//     Console.WriteLine(m);
+// else
+//     Console.WriteLine(m);
+
+// do-while, while, for(foreach)
+
+// int i = 1;
+// while (i < 10)
+// {
+//     Console.Write($"{i} ");
+//     i++; // i = i + 1;
+// }
+
+// int i = 1;
+// do{
+//     Console.Write($"{i} ");
+//     i++;
+// }
+// while (i < 10);
+
+// For
+// for (begin;condition;step)
+
+// for(int i = 0; i < 10; i++)
+//     Console.Write($"{i} ");
+
+// Console.WriteLine();
+
+// for(int i = 10; i > 0; i--)
+//     Console.Write($"{i} ");
+
+// Console.WriteLine();
+
+// for(int i = 0; i < 10; i+=2)
+//     Console.Write($"{i} ");
