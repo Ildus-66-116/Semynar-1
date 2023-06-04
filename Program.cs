adca8fc29716b5c58e94e1238b8f0d697c1548f6
ILDUS <ildus6696@gmail.com>
2023-06-04 15:02:29 +0300

    Seminar 6

--- Program.cs
+++ Program.cs
@@ -555,3 +555,97 @@ Console.WriteLine("Hello");
 // double[] array = new double[n];
 // InputArray(array);
 // Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+
+
+// Семинар 6 Одномерные массивы. Продолжение
+
+// Задача 39
+
+// void InputArray(double[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = Math.Round(new Random().NextDouble() * (50 - 10) + 10, 2);
+// }
+
+
+// void ReverseArray(double[] array)
+// {
+//     for (int i = 0; i < array.Length / 2; i++)
+//     {
+//         double temp = array[i];
+//         array[i] = array[array.Length - 1 - i];
+//         array[array.Length - 1 - i] = temp;
+//     }
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// double[] array = new double[n];
+// InputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join("; ", array)}]");
+// ReverseArray(array);
+// Console.WriteLine($"Конечный массив: [{string.Join("; ", array)}]");
+
+// Задача 40
+
+// Console.Clear();
+// Console.Write("Введите 1-ую сторону: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите 2-ую сторону: ");
+// int b = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите 3-ю сторону: ");
+// int c = int.Parse(Console.ReadLine()!);
+// if (a < b + c && b < a + c && c < a + b)
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
+
+// Задача 42 Перевод в двоичную
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// string result = string.Empty;
+// while (n > 0)
+// {
+//     result = $"{n % 2}" + result;
+//     n /= 2;
+// }
+// Console.WriteLine(result);
+
+// Задача 44 Как быстро делать теорию Фибаначи
+
+
+// int fib(int n)
+// {
+//     if (n == 0 || n == 1)
+//         return 1;
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!), a0 = 0, a1 = 1, x;
+// //Console.WriteLine(fib(n));
+// for (int i = 0; i < n; i++)
+// {
+//     Console.Write($"{a0} ");
+//     x = a0 + a1;
+//     a0 = a1;
+//     a1 = x;
+// }
+
+// Задача 45 копирование
+
+Console.Clear();
+int[] a = {1, 2, 3, 4, 5};
+int[] b = new int[a.Length];
+
+for (int i = 0; i < b.Length; i++)
+    b[i] = a[i];
+    
+b[0] = b[0] + 5;
+Console.WriteLine(string.Join(" ", a));
+Console.WriteLine(string.Join(" ", b));
